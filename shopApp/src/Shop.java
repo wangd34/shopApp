a51cec2f57890f0523e156ac983ca730343c208c
zhaoyuchennn <64751474+zhaoyuchennn@users.noreply.github.com>
2021-04-01 22:23:45 +0800

    shop class with sort goods

--- shopApp/src/Shop.java
+++ shopApp/src/Shop.java
@@ -11,7 +11,7 @@ public class Shop {
 	
 	 static ArrayList<Product> prodInf = new ArrayList<>();
 	 
-	public void addProduct() {
+	public void addProduct() throws IOException {
 		System.out.println("Which product do you want to add?");
 		System.out.println("Please enter following four requirments: Name, Kind, Num, Price.");
 		Product prod = new Product(getName(), getKind(), getNum(), getPrice());
@@ -22,6 +22,48 @@ public class Shop {
             System.out.println("Added Failed!");
         }
         System.out.print(prodInf.toString());
+        
+        File myObj = new File("CommodityList.txt");
+        Scanner myReader = new Scanner(myObj); 
+        String data;
+        while (myReader.hasNextLine()) {
+          data = myReader.nextLine();
+          System.out.println(data);
+          if (data.contains("fruit")) {
+        	  File file1 = new File("fruit.txt");
+              FileWriter fw1 = new FileWriter(file1,true);
+              BufferedWriter bw1 = new BufferedWriter(fw1);
+              bw1.write(prodInf.toString());
+              bw1.newLine();
+              bw1.close();
+          }
+          if (data.contains("drinks")) {
+        	  File file2 = new File("drinks.txt");
+              FileWriter fw2 = new FileWriter(file2,true);
+              BufferedWriter bw2 = new BufferedWriter(fw2);
+              bw2.write(prodInf.toString());
+              bw2.newLine();
+              bw2.close();
+          }
+          if (data.contains("meat")) {
+        	  File file3 = new File("meat.txt");
+              FileWriter fw3 = new FileWriter(file3,true);
+              BufferedWriter bw3 = new BufferedWriter(fw3);
+              bw3.write(prodInf.toString());
+              bw3.newLine();
+              bw3.close();
+          }
+          if (data.contains("spices")) {
+        	  File file4 = new File("spices.txt");
+              FileWriter fw4 = new FileWriter(file4,true);
+              BufferedWriter bw4 = new BufferedWriter(fw4);
+              bw4.write(prodInf.toString());
+              bw4.newLine();
+              bw4.close();
+          }
+          
+        }
+        myReader.close();
 		
 	}
 	
